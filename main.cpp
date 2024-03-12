daf666256594518cb29d7dd8148a5642c23e4d8f
fonderoo <153612995+fonderoo@users.noreply.github.com>
2024-03-12 13:43:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void print_1(const int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << 1;
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Каламбет Данила Сергеевич Группа: 090301 - ПОВа - о23" << endl;
+    int N;
+    cin >> N;
+    if (N < 0)
+    {
+        cout << "NO";
+        return 0;
+    }
+    if (N >= 1)
+    {
+        if (N > pow(10, 6))
+        {
+            cout << "NO";
+        }
+        if (N <= pow(10, 6))
+        {
+            if (N % 2 == 0)
+            {
+                cout << "NO";
+                return 0;
+            }
+            if (N % 5 == 0)
+            {
+                cout << "NO";
+                return 0;
+            }
+
+            if (N == 1)
+            {
+                cout << 1;
+                return 0;
+            }
+
+            long int A = 1;
+            for (long int i = 2; i <= N; i++)
+            {
+                A = (A * 10 + 1) % N;
+                if (A == 0)
+                {
+                    print_1(i);
+                    return 0;
+                }
+                if (A == 1)
+                {
+                    print_1(i - 1);
+                    return 0;
+                }
+
+            }
+        }
+    }
+}
